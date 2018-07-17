b6c73c2adad5e5bc94398707b52c2d91acc5943f
Carlos Augusto Aragón Cruz <ing.c.aragon@gmail.com>
2018-07-17 17:05:55 -0500

    Agregando linea para el login al docker contain de google

--- publish.sh
+++ publish.sh
@@ -1,4 +1,5 @@
 gradle clean build
 docker build -t ca/examplerest:0.0.1 .
 docker tag ca/examplerest:0.0.1 gcr.io/istio-001/examplerest:0.0.1
+docker login -u oauth2accesstoken -p "$(gcloud auth print-access-token)" https://gcr.io
 docker push gcr.io/istio-001/examplerest:0.0.1
